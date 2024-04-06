d7953690c3be6e3049636ded45e04aa9899ad1f0
Agus Prayogi <agus21apy@gmail.com>
2024-04-06 21:13:05 +0700

    [ADD] state management and theme router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,66 @@
-import React from 'react';
-import {Stack} from "expo-router";
-import {GluestackUIProvider} from "@gluestack-ui/themed";
-import {config} from "@gluestack-ui/config";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
-function _Layout() {
-	return (
-		<GluestackUIProvider config={config}>
-			<Stack>
-				<Stack.Screen name="index"/>
-			</Stack>
-		</GluestackUIProvider>
-	);
+import { useColorScheme } from "@/components/useColorScheme";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "(tabs)",
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
 }
 
-export default _Layout;
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <GluestackUIProvider config={config}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack>
+      </ThemeProvider>
+    </GluestackUIProvider>
+  );
+}
